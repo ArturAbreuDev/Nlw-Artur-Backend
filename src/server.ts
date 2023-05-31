12ed03e62381e2b550b239b34e95768464f27bcc
Artur Damaceno Abreu <115594906+ArturAbreuDev@users.noreply.github.com>
2023-05-31 12:28:48 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,50 @@
-import fastify from 'fastify'
+import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { memoriesRoutes } from './routes/memories'
-import 'dotenv/config'
-import { authRoutes } from './routes/auth'
 import jwt from '@fastify/jwt'
 import multipart from '@fastify/multipart'
+import staticLib from '@fastify/static'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 import { uploadRoutes } from './routes/upload'
-import { resolve } from 'node:path'
+import { resolve } from 'path'
+import { env } from 'process'
 
-const app = fastify()
-
-app.register(multipart)
-app.register(require('@fastify/static'), {
-  root: resolve(__dirname, '../uploads'),
-  prefix: '/uploads',
-})
-app.register(cors, {
-  origin: true,
-})
-app.register(uploadRoutes)
-app.register(jwt, {
-  secret: 'spacetime',
-})
-app.register(authRoutes)
-
-app.register(memoriesRoutes)
-
-app
-  .listen({
-    port: 3333,
-    host: '0.0.0.0',
+export function init() {
+  const app = Fastify({
+    logger: false,
   })
-  .then(() => {
-    console.log('🚀 HTTP server running on http://localhost:3333')
+
+  app.register(multipart)
+
+  app.register(cors, {
+    origin: [
+      'https://nlw-artur-2gzf5f220-arturabreudev.vercel.app/',
+      'http://localhost:3000',
+    ],
   })
+
+  app.register(jwt, {
+    secret: `${env.SECRET}`,
+  })
+
+  app.register(memoriesRoutes)
+  app.register(authRoutes)
+  app.register(uploadRoutes)
+  app.register(staticLib, {
+    root: resolve(__dirname, '../uploads'),
+    prefix: '/uploads',
+  })
+
+  return app
+}
+
+// Execute the app when called directly( ex.: "npm run dev")
+if (require.main === module) {
+  init()
+    .listen({
+      port: 3333,
+    })
+    .then(() => {
+      console.log('Running on port 3333')
+    })
+}
